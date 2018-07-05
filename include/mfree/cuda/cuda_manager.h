812673aeaafa94c097625d1ef417df047c044a73
Julian Merten <julian.merten@physics.ox.ac.uk>
2018-07-05 10:26:03 +0200

    Initial cuda commit.

---
+++ include/mfree/cuda/cuda_manager.h
@@ -0,0 +1,19 @@
+/*** /cuda/cuda_manager.h
+     This provides the base class
+     which handles CUDA device and mainly
+     memory management. 
+
+Julian Merten
+INAF OA Bologna
+July 2018
+julian.merten@inaf.it
+http://www.julianmerten.net
+***/
+
+#ifndef    CUDA_MANAGER_H
+#define    CUDA_MANAGER_H
+
+
+
+
+#endif /* CUDA_MANAGER_H */
